803b180525402eac3d0a154347b2482bb0301db3
ghazi-naceur <ghazi.ennacer@gmail.com>
2024-02-08 23:27:37 +0100

    Add http server

---
+++ backend/src/main/scala-3/in/oss/docker/manager/Main.scala
@@ -0,0 +1,26 @@
+package in.oss.docker.manager
+
+import cats.effect.{IO, IOApp}
+import com.comcast.ip4s.{host, port}
+import in.oss.docker.manager.config.AppConfig
+import in.oss.docker.manager.controller.DockerController
+import org.http4s.ember.server.EmberServerBuilder
+import pureconfig.ConfigSource
+import in.oss.docker.manager.config.Syntax.*
+
+object Main extends IOApp.Simple {
+
+  override def run: IO[Unit] = {
+    ConfigSource.default
+      .loadF[IO, AppConfig]
+      .flatMap { case AppConfig(emberServerConfig) =>
+        EmberServerBuilder
+          .default[IO]
+          .withHost(emberServerConfig.host)
+          .withPort(emberServerConfig.port)
+          .withHttpApp(DockerController[IO]().routes)
+          .build
+          .use(_ => IO.println("Docker Manager started...") *> IO.never)
+      }
+  }
+}
