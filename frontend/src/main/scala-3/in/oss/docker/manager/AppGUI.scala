2ce665a1eb28c940394290c57e06797beab90916
ghazi-naceur <ghazi.ennacer@gmail.com>
2024-02-09 12:31:10 +0100

    Display containers in FE

--- frontend/src/main/scala-3/in/oss/docker/manager/AppGUI.scala
+++ frontend/src/main/scala-3/in/oss/docker/manager/AppGUI.scala
@@ -1,29 +1,67 @@
 package in.oss.docker.manager
 
-import cats.effect.IO
-import in.oss.docker.manager.AppGUI.{Message, Model}
+import cats.effect.*
+import in.oss.docker.manager.domain.Container
+import io.circe.parser.*
+import io.circe.generic.auto.*
 import tyrian.*
 import tyrian.Html.*
+import tyrian.http.*
 import tyrian.{Cmd, Html, Sub, TyrianApp}
 
+import scala.scalajs.js
 import scala.scalajs.js.annotation.JSExportTopLevel
 
-@JSExportTopLevel("DockerManagerGUI")
-class AppGUI extends TyrianApp[Message, Model] {
-  override def init(flags: Map[String, String]): (Model, Cmd[IO, Message]) =
-    (Model(""), Cmd.None)
-
-  override def update(model: Model): Message => (Model, Cmd[IO, Message]) = ???
-
-  override def view(model: Model): Html[Message] =
-    div(
-      p("Docker Manager")
-    )
-
-  override def subscriptions(model: Model): Sub[IO, Message] = ???
+enum Message {
+  case NoOp
+  case LoadContainers(containers: List[Container])
+  case Error(error: String)
 }
 
-object AppGUI {
-  sealed trait Message
-  case class Model(data: String)
+case class Model(containers: List[Container])
+
+@JSExportTopLevel("DockerManagerGUI")
+object AppGUI extends TyrianApp[Message, Model] {
+  override def init(flags: Map[String, String]): (Model, Cmd[IO, Message]) =
+    (Model(List()), getContainersEndpoint)
+
+  override def view(model: Model): Html[Message] = {
+    div(
+      h1("Containers"),
+      div(
+        model.containers.map { container =>
+          div(
+            div(label("==========================")),
+            div(strong("Container ID: "), label(container.containerId.value)),
+            div(strong("Command: "), label(container.command.value)),
+            div(strong("Image: "), label(container.image.value)),
+            div(strong("Ports: "), label(container.ports.value)),
+            div(strong("Created: "), label(container.created.value)),
+            div(strong("Names: "), label(container.names.value)),
+            div(strong("Status: "), label(container.status.value))
+          )
+        }
+      )
+    )
+  }
+
+  override def update(model: Model): Message => (Model, Cmd[IO, Message]) = {
+    case Message.LoadContainers(containers) => (model.copy(containers = model.containers ++ containers), Cmd.None)
+    case Message.NoOp                       => (model, Cmd.None)
+    case Message.Error(error)               => (model, Cmd.None)
+  }
+
+  override def subscriptions(model: Model): Sub[IO, Message] = Sub.None
+
+  private def getContainersEndpoint: Cmd[IO, Message] = Http.send(
+    Request.get("http://localhost:5555/docker/containers"),
+    Decoder[Message](
+      response =>
+        parse(response.body).flatMap(_.as[List[Container]]) match {
+          case Right(containers) => Message.LoadContainers(containers)
+          case Left(thr)         => Message.Error(thr.getMessage)
+        },
+      error => Message.Error(error.toString)
+    )
+  )
 }
